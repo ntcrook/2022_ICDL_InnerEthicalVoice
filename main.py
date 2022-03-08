bdeabcea7da9f9409e99019af0747c9e75cb3174
Nigel Crook <ncrook@brookes.ac.uk>
2022-03-08 07:15:50 +0000

    Code for 2D positional embedding added.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-print("hello")
+import numpy as np
+import math
 
-print("why isn't this working")
+def positionalEmbedding2D(x,y,embeddingDim):
+    D = float(embeddingDim)
+    embedding = np.empty(embeddingDim)
+    for i in range(0,embeddingDim//2):
+        if i % 2 == 0:
+            embedding[i] = math.cos(x/10000**(4*i/D))
+            embedding[i + embeddingDim//2] = math.cos(y/10000**(4*i/D))
+        else:
+            embedding[i] = math.sin(x/10000**(4*i/D))
+            embedding[i + embeddingDim//2] = math.sin(y/10000**(4*i/D))
+    return embedding
 
-print("adding this")
+e = positionalEmbedding2D(10,20,512)
+np.savetxt("embeddingTest.csv", e, delimiter = ",")
+
+print('Embedding saved')
